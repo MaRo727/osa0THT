5744fa7a4b5f5e8e6ede5389c5fbeabab868be56
MaRo727 <115220286+MaRo727@users.noreply.github.com>
2023-09-04 10:00:09 +0300

    2.6 kesken

--- 2.2/src/App.jsx
+++ 2.2/src/App.jsx
@@ -6,22 +6,32 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+const handleNames = () => {
+  setNewName(newName).push(handleList)
+}
+
+const handleList = () => {
+  setPersons(persons)
+}
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input {...handleNames}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <p>{persons}</p>
     </div>
   )
 
 }
 
 export default App
+
+// 
